fa381025e503f48b9a7662a7a4d62f9e78dce83e
kafri89 <monafkafri@gmail.com>
2025-05-11 22:29:09 +0300

    Update sw.js

--- www/sw.js
+++ www/sw.js
@@ -1,9 +1,19 @@
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/my-html-app/www/sw.js')
-    .then(function(reg) {
-      console.log('Service Worker registered.', reg);
+self.addEventListener('install', function (e) {
+  e.waitUntil(
+    caches.open('html-app-cache').then(function (cache) {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/manifest.json'
+      ]);
     })
-    .catch(function(err) {
-      console.log('Service Worker registration failed:', err);
-    });
-}
+  );
+});
+
+self.addEventListener('fetch', function (e) {
+  e.respondWith(
+    caches.match(e.request).then(function (response) {
+      return response || fetch(e.request);
+    })
+  );
+});
