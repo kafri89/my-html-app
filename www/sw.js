59a9a8b37796c8de14a42e63a58ff80de795dd46
kafri89 <monafkafri@gmail.com>
2025-05-11 22:40:44 +0300

    Update sw.js

--- www/sw.js
+++ www/sw.js
@@ -1,19 +1,18 @@
-self.addEventListener('install', function (e) {
+self.addEventListener('install', (e) => {
   e.waitUntil(
-    caches.open('html-app-cache').then(function (cache) {
+    caches.open('html-app-cache').then((cache) => {
       return cache.addAll([
-        '/',
-        '/index.html',
-        '/manifest.json'
+        '/my-html-app/www/index.html',
+        '/my-html-app/www/manifest.json',
+        '/my-html-app/www/icon-192.png',
+        '/my-html-app/www/icon-512.png',
+        '/my-html-app/www/Cats Show.ttf',
+        '/my-html-app/www/KOMIKAX_.ttf',
+        '/my-html-app/www/Komigo3D.woff',
+        '/my-html-app/www/block.mp3',
+        '/my-html-app/www/win.mp3',
+        
       ]);
-    })
-  );
-});
-
-self.addEventListener('fetch', function (e) {
-  e.respondWith(
-    caches.match(e.request).then(function (response) {
-      return response || fetch(e.request);
     })
   );
 });
